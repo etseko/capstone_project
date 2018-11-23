2dfee5b237520f8da61f01d55676b20cca69f326
Eleana Tsekoglou <etseko@essex.ac.uk>
2018-11-23 16:55:00 +0000

    rearraged the routes for the new views related to the meters

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   get 'static_pages/about'
   get '/index', to: 'home#index'
+  get '/show', to: 'meters#show'
+  get '/meters/new' , to: 'meters#new'
+  post '/meters/new', to: 'meters#create'
 end
