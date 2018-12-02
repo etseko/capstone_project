f8099a529b3c22fa247b077bc55aba57d82ad710
Eleana Tsekoglou <etseko@essex.ac.uk>
2018-12-02 15:19:09 +0000

    set the routes for the new appliances pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :appliances
   root :to => 'home#index'
   get 'static_pages/about'
   get '/index', to: 'home#index'
-  get '/show', to: 'meters#show'
+  get 'meters/show', to: 'meters#show'
   get '/meters/new' , to: 'meters#new'
   post '/meters/new', to: 'meters#create'
+  get 'appliances/show', to:'appliances#show'
 end
