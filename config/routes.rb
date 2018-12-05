47a9b03163715c39fa31db866a3cb5d49f6d0b5b
Eleana Tsekoglou <etseko@essex.ac.uk>
2018-12-05 20:13:28 +0000

    Changed static_pages controller function from about to index, changed the name of the view to index and corrected the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :meters
   resources :appliances
+  resources :static_pages
   root :to => 'home#index'
   get 'static_pages/about'
   get '/index', to: 'home#index'
