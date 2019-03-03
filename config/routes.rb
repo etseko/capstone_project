4e9630a05f807dc7e419a36563dc997967b0d4b5
Eleana Tsekoglou <etseko@essex.ac.uk>
2019-03-03 19:27:46 +0000

    Devise user is created along with its views test and model

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  post '/meters/create_auto', to: 'meters#create_auto'
   resources :meters
   resources :appliances
   resources :static_pages
   root :to => 'home#index'
   get 'static_pages/about'
   get '/index', to: 'home#index'
-  post 'meters/create_auto', to: 'meters#create_auto'
 end
