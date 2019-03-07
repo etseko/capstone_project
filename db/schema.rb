14c2ffe25c2704c022a3df65bc69ba2ea9d943fc
Eleana Tsekoglou <etseko@essex.ac.uk>
2019-03-07 14:19:57 +0000

    added image attribute to the User

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_04_161431) do
+ActiveRecord::Schema.define(version: 2019_03_05_194646) do
 
   create_table "appliances", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "model"
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_03_04_161431) do
     t.string "surname"
     t.string "address"
     t.string "auth_token"
+    t.string "image"
     t.index ["auth_token"], name: "index_users_on_auth_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
